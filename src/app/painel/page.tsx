1a94966620228e5ef14c90198e60746cbab0e762
João Marcos Alecsandro Kirimis <150600077+jmkirimis@users.noreply.github.com>
2025-09-12 13:34:15 -0300

    feat(pages): adiciona navbar na página inicial do painel

--- src/app/painel/page.tsx
+++ src/app/painel/page.tsx
@@ -1,5 +1,15 @@
+import Navbar from "@/components/painel/Navbar";
+
 export default function Painel() {
     return (
-        <div></div>
+        <div className="flex">
+            <Navbar />
+            <main className="flex-1 p-6 bg-white text-zinc-800">
+                <div className="p-6">
+                    {/* Boas-vindas */}
+                    <h1 className="text-2xl font-bold mb-4">Bem-vindo de volta, João 👋</h1>
+                </div>
+            </main>
+        </div>
     );
 }
